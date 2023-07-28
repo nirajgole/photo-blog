76a6bb2669c611fce014895beac2483e80093c47
nirajgole <nirajgp777@gmail.com>
2023-07-28 10:01:30 +0530

    fixed cors and set-cookie on frontend

--- react_frontend/src/auth/authAPI.ts
+++ react_frontend/src/auth/authAPI.ts
@@ -7,7 +7,7 @@ const client = axios.create({
   headers: {
     // 'Accept':'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': 'localhost'
   },
   withCredentials: true
 })
